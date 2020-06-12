04dd4c4c6ede47337162271edb3d57fcf1c5b172
Anubhav <18ucs066@lnmiit.ac.in>
2020-06-12 17:34:40 +0530

    ypyp

--- app/src/main/java/lnmiit/college/counsellingapp/LoginActivity.java
+++ app/src/main/java/lnmiit/college/counsellingapp/LoginActivity.java
@@ -3,6 +3,7 @@ package lnmiit.college.counsellingapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,12 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import lnmiit.college.counsellingapp.mn.crawler.rview.MainActivity;
 
@@ -25,26 +34,76 @@ import static android.widget.Toast.*;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
+    private EditText student_username,student_password,faculty_username,faculty_password,username,password,type;
+
     private FirebaseAuth mauth;
-    private ImageButton login;
     private SharedPreferences prefs = null;
-    @Override
+    private ImageButton login_as_student,login_as_faculty,signup,student_login,faculty_login,signin;
+    private LinearLayout student_layout,faculty_layout,signup_layout,choice_layout;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore ff;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ff=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseUser=firebaseAuth.getCurrentUser();
         mauth=FirebaseAuth.getInstance();
-        username=findViewById(R.id.Username);
-        password=findViewById(R.id.Password);
-        login=findViewById(R.id.Login);
+
+        choice_layout=findViewById(R.id.choiceLinearLayout);
+        signup_layout=findViewById(R.id.signupLayout);
+        student_layout=findViewById(R.id.studentLinearLayout);
+        faculty_layout=findViewById(R.id.facultyLinearLayout);
+
+        student_username=findViewById(R.id.studentUsername);
+        faculty_username=findViewById(R.id.facultyUsername);
+        student_password=findViewById(R.id.studentPassword);
+        faculty_password=findViewById(R.id.facultyPassword);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        type=findViewById(R.id.type);
+
+
+        signup=findViewById(R.id.signup);
+        login_as_student=findViewById(R.id.loginAsStudent);
+        login_as_faculty=findViewById(R.id.loginAsFaculty);
+
+        signin=findViewById(R.id.signin);
+        faculty_login=findViewById(R.id.facultyLogin);
+        student_login=findViewById(R.id.studentLogin);
+
+        login_as_student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                student_layout.setVisibility(View.VISIBLE);
+                choice_layout.setVisibility(View.GONE);
+            }
+        });
+        login_as_faculty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faculty_layout.setVisibility(View.VISIBLE);
+                choice_layout.setVisibility(View.GONE);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup_layout.setVisibility(View.VISIBLE);
+                choice_layout.setVisibility(View.GONE);
+            }
+        });
         prefs = getSharedPreferences("lnmiit.college.counsellingapp",MODE_PRIVATE);
-        login.setOnClickListener(new View.OnClickListener() {
+        student_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this,"","Please Wait");
-                mauth.signInWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mauth.signInWithEmailAndPassword(student_username.getText().toString(),student_password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
@@ -62,6 +121,64 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
+        faculty_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this,"","Please Wait");
+                mauth.signInWithEmailAndPassword(faculty_username.getText().toString(),faculty_password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            ff.collection("users").document(firebaseUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    DocumentSnapshot queryDocumentSnapshot=task.getResult();
+                                    String type= queryDocumentSnapshot.getString("type");
+                                    if(type=="faculty"){
+                                        Toast.makeText(LoginActivity.this, "signin successful", LENGTH_LONG).show();
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        progressDialog.dismiss();
+                                        finish();
+                                    }
+                                    else{
+                                        firebaseUser=null;
+                                        Toast.makeText(LoginActivity.this, "signin failed", LENGTH_LONG).show();
+                                        progressDialog.dismiss();
+                                    }
+                                }
+                            });
+
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "signin failed", LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.createUserWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Map<String,Object>map=new HashMap<>();
+                            map.put("type",type.getText().toString());
+                            ff.collection("users").document(firebaseUser.getUid()).set(map);
+                            Toast.makeText(LoginActivity.this,"done", LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"error", LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
 
 
     }
